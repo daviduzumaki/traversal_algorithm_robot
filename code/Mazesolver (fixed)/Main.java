bebeb5cc78bba25ff472bdece3f088797ef8faff
David Ajayi <david.ajayi2@mail.dcu.ie>
2018-02-19 00:33:53 +0000

    main form of the mazesolver (window size, visible, re-sizable, etc)

---
+++ code/Mazesolver (fixed)/Main.java
@@ -0,0 +1,34 @@
+package mazesolver;
+
+
+import javax.swing.JFrame;
+
+
+
+
+public class Main {
+
+    public static JFrame mazeFrame;  // The main form of the program
+  
+    
+    public static void main(String[] args) {
+        int width  = 800;
+        int height = 1200;
+        mazeFrame = new JFrame("Maze Search by David And Ore");
+        //The area where the grid is located
+        mazeFrame.setContentPane(new MazePanel(width,height));
+        mazeFrame.pack();
+        mazeFrame.setResizable(false);
+        //the form is located in the center of the screen
+        mazeFrame.setLocationRelativeTo(null);
+        mazeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mazeFrame.setVisible(true);
+        
+    } // end main()
+    
+    
+ 
+  
+    
+    
+} // end class Maze
