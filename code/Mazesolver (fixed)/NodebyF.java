703e0596715ff60ff1a44cb4008590b7e1c223d1
David Ajayi <david.ajayi2@mail.dcu.ie>
2018-02-20 02:48:38 +0000

    Update NodebyF.java

--- code/Mazesolver (fixed)/NodebyF.java
+++ code/Mazesolver (fixed)/NodebyF.java
@@ -1,13 +1,11 @@
-
 package mazesolver;
 
 import java.util.Comparator;
 
 
-     // by default the compare method compares integers. We're overriding
-     // it so it compares the F value returned by two nodes instead which are doubles.
-     // even if it is not overridden it will still execute however this improves accuracy. 
-
+   /*
+   * Comparator aid provided by: https://stackoverflow.com/questions/16425127/how-to-use-collections-sort-in-java-specific-situation
+   */
 
 public class NodebyF implements Comparator<Node>{
        
