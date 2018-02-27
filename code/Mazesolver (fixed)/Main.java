f039a515cd9dda6b9f1b69c202fd5eced71f7fa4
David Ajayi <david.ajayi2@mail.dcu.ie>
2018-02-27 02:22:06 +0000

    The program now changes size depending on the screen resolution of the monitor

--- code/Mazesolver (fixed)/Main.java
+++ code/Mazesolver (fixed)/Main.java
@@ -2,9 +2,14 @@ package mazesolver;
 
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
-
+/** 
+ * 
+ * @author 8fact
+ */
 
 
 public class Main {
@@ -13,14 +18,16 @@ public class Main {
   
     
     public static void main(String[] args) {
-        int width  = 800;
-        int height = 1200;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width  = (int) screenSize.getWidth()/4;
+        int height = (int) ((int) screenSize.getHeight()/1.2);
+        
         mazeFrame = new JFrame("Maze Search by David And Ore");
         //The area where the grid is located
         mazeFrame.setContentPane(new mazeUI(width,height));
         mazeFrame.getContentPane().setBackground(Color.DARK_GRAY.darker());
         mazeFrame.pack(); 
-        mazeFrame.setResizable(false);
+       // mazeFrame.setResizable(false);
         //it loads on the centre of the screen
         mazeFrame.setLocationRelativeTo(null);
         mazeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
